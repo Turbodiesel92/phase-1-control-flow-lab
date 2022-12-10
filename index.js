11774a0b94cc3ab3039da660ab6e59498206e49d
Matthew <turbodieseldodge@gmail.com>
2022-12-10 13:55:30 -0600

    commit tree 22c30ea022c85db9505969b469b238d614469cda

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  const shortDistance = 400
+  const mediumDistance = 2000
+  const longDistance = 2500
+
+  if (distance <= shortDistance) {
+    return 'This one is on me!'
+  } else if (distance > shortDistance && distance <= mediumDistance) {
+    return 'That will be twenty bucks.'
+  } else if (distance > mediumDistance && distance <= longDistance) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (distance > longDistance) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
+  }
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
