06fd32088d6aa895dfdb3ea071a56a1fb8d7b816
Matthew <turbodieseldodge@gmail.com>
2023-01-07 15:12:38 -0600

    retake study

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(yourRide){
+  const shortRide = 400
+  const mediumRide = 2000
+  const longRide = 2500
+
+  if(yourRide <= shortRide){
+    return 'This one is on me!'
+  } else if(yourRide > shortRide && yourRide <= mediumRide) {
+    return 'That will be twenty bucks.'
+  } else if(yourRide > mediumRide && yourRide <= longRide) {
+    return 'I will gladly take your thirty bucks.'
+  } else if(yourRide > longRide) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
